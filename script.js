7758c54df3744e7da98cb5210bb63ea5e3c1151d
Nahuel Berensztein <146759546+nahuelberensztein@users.noreply.github.com>
2024-06-19 22:24:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let nombre = prompt("Ingrese su nombre:");
 let edad = prompt("Ingrese su edad:");
 let fechaDeNacimiento = prompt("Ingrese su año de nacimiento:");
 
-fechaDeNacimiento = (2024 - fechaDeNacimiento) * 365;
+let diasDeNacimiento = (2024 - fechaDeNacimiento) * 365;
 
-alert("Nombre: " + nombre + "\nEdad: " + edad + "\nFecha de Nacimiento: " + fechaDeNacimiento + "\nDías vividos aproximados: " + fechaDeNacimiento);
+alert("Nombre: " + nombre + "\nEdad: " + edad + "\nFecha de Nacimiento: " + fechaDeNacimiento + "\nDías vividos aproximados: " + diaDeNacimiento);
